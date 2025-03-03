ce5a5be77a8a89ccaac027f555e3e5122eaf59f7
John Sims <johnsims.dev@gmail.com>
2025-03-03 16:14:20 -0500

    add circle shape, add player

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
-
-# from database import connect_database, database_version
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        #  limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
